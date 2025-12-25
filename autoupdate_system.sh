bf79715862bfc15864626851fbb09f775008c9e2
ramses2099 <ramses2099@gmail.com>
2025-12-25 08:08:08 -0400

    changes in auto update system

--- autoupdate_system.sh
+++ autoupdate_system.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
-echo "Auto-Update System"
+
+# Define color variables
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+BLUE='\033[0;34m'
+NC='\033[0m' # No Color (reset)
+
+echo -e "${GREEN}Staring Auto-Update System${NC}"
 
 sudo apt update
 
@@ -11,3 +19,7 @@ apt list --upgradable
 clear
 
 sudo apt upgrade -y
+
+clear
+
+echo -e "${GREEN}System update successfully${NC}"
